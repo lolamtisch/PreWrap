b1eeb48821a8dd99da5fed8fa0fabf11d89b2f50
lolamtisch <francisco.seipel@yahoo.de>
2020-09-25 09:42:06 +0200

    [TASK] Add packing

---
+++ package/updateVersion.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+
+var manifest = path.join(__dirname, "..", "Extension", "manifest.json");
+fs.readFile(manifest, "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const ver = new Date().toISOString().replace(/T.*/, "").replace(/-/g, ".");
+
+  const result = data.replace(
+    /"version": "[\d|\.]+",/g,
+    `"version": "${ver}",`
+  );
+
+  fs.writeFile(manifest, result, "utf8", function (err) {
+    if (err) return console.log(err);
+  });
+});
