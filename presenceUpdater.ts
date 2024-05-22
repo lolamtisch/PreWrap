696a7fd4a5b94d2c99634d7e2cfb3aa0f70b0f09
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-05-22 16:48:07 +0200

    [BUGFIX] Handle regex

--- presenceUpdater.ts
+++ presenceUpdater.ts
@@ -1,6 +1,7 @@
 const regExpReplacement = {
   'Amazon': '([a-z0-9-]+[.])*amazon([.][a-z]+)+[/]',
   'eggsy.codes': 'eggsy[.]xyz',
+  'IDLIX': '(((tv([0-9]?))?(vip)?[.])?id(f)?lix(official)?[.][a-z]{2,6})',
 }
 
 
